b18425a22217b67e14ff292feed3e93927f60a18
Lucky Chauhan <159449655+luckychauhan8218@users.noreply.github.com>
2026-01-30 17:28:37 +0530

    Sort the Array

---
+++ basic/SortArray.java
@@ -0,0 +1,23 @@
+class SortArray {
+    public static void main(String[] args) {
+        int arr[]={5,2,8,7,1};
+        int temp=0;
+        System.out.println("Original array is");
+        for(int i=0;i<arr.length;i++)
+        System.out.print(arr[i]+" ");
+        
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[i]){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.println("  ");
+         System.out.println("Sorted array is");
+        for(int i=0;i<arr.length;i++)
+        System.out.print(arr[i]+" ");
+    }
+}
