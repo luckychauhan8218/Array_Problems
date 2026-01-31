16a27014d005c8b7690adc806da0a1c2b4a9dd5a
Lucky Chauhan <159449655+luckychauhan8218@users.noreply.github.com>
2026-01-31 22:14:54 +0530

    final Update AddTwoMatrix

--- AddTwoMatrix.java
+++ AddTwoMatrix.java
@@ -1,3 +1,6 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
 import java.util.Scanner;
 class AddTwoMatrix {
     public static void main(String[] args) {
@@ -23,14 +26,14 @@ class AddTwoMatrix {
        // Add matrices
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                sum[i][j] = a[i][j] + b[i][j];
+                c[i][j] = a[i][j] + b[i][j];
             }
         }
        // Print result
         System.out.println("Sum of matrices:");
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                System.out.print(sum[i][j] + " ");
+                System.out.print(c[i][j] + " ");
             }
             System.out.println();
         }
