cfe1c2e319cb15a91f13222645069e17e2e3d13a
Lucky Chauhan <159449655+luckychauhan8218@users.noreply.github.com>
2026-01-31 22:11:44 +0530

    Update AddTwoMatrix

--- AddTwoMatrix.java
+++ AddTwoMatrix.java
@@ -6,39 +6,34 @@ class AddTwoMatrix {
         int c[][]=new int [2][2];
         
         Scanner r=new Scanner(System.in);
-        System.out.println("Enter first matrix Data");
-        for(int i=0;i<2;i++){
-            for(int j=0;j<2;j++){
-                a[i][j]=r.nextInt();
+        // Input first matrix
+        System.out.println("Enter first matrix elements:");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                a[i][j] = sc.nextInt();
             }
         }
-        System.out.println("Enter second matrix Data");
-        for(int i=0;i<2;i++){
-            for(int j=0;j<2;j++){
-                b[i][j]=r.nextInt();
+       // Input second matrix
+        System.out.println("Enter second matrix elements:");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                b[i][j] = sc.nextInt();
             }
         }
-        System.out.println("First Matrix \n");
-         for(int i=0;i<2;i++){
-            for(int j=0;j<2;j++){
-                System.out.print(a[i][j]+" ");
+       // Add matrices
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                sum[i][j] = a[i][j] + b[i][j];
             }
-            System.out.print("\n");
         }
-        System.out.println("Second Matrix \n");
-         for(int i=0;i<2;i++){
-            for(int j=0;j<2;j++){
-                System.out.print(b[i][j]+" ");
+       // Print result
+        System.out.println("Sum of matrices:");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(sum[i][j] + " ");
             }
-            System.out.print("\n");
+            System.out.println();
         }
-        System.out.print("Some of Two Matrix \n");
-         for(int i=0;i<2;i++){
-            for(int j=0;j<2;j++){
-                c[i][j]=a[i][j]+b[i][j];
-                System.out.print(c[i][j]+" "); 
-            }
-            System.out.print("\n");
-        }
+       
     }
 }
